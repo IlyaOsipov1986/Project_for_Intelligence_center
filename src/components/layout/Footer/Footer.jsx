8843e051a67ff680e400112aba3290dd466aef66
Осипов Илья Иванович <st807785@ad.pu.ru>
2025-01-20 15:55:58 +0300

    fix style header + footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,11 +1,21 @@
 import '../Footer/Footer.scss';
+import React from "react";
 import FooterBottom from './FooterBottom';
-import FooterTop from './FooterTop';
+import { footerData  } from "../../../config";
+import FooterCard from '../Footer/FooterCard';
 
 const Footer = () => {
     return (
         <div className='footer-container'>
-           <FooterTop/>
+            {footerData.map((el) => {
+            return (
+                <React.Fragment key={el.id}>
+                    <FooterCard
+                        {...el}
+                    />
+                </React.Fragment>
+                )
+            })}
            <FooterBottom/>
         </div>
     )
